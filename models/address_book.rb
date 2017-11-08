2dfa6e0bb6d5211c2bcafca9a33a77a8d07c10bd
Tucker Platt <tucker@bendcable.com>
2017-11-08 18:30:36 +0000

    checkpoint work finished

--- models/address_book.rb
+++ models/address_book.rb
@@ -34,4 +34,31 @@ class AddressBook
        add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
      end
     end
+    
+    # Search AddressBook for a specific entry by name
+   def binary_search(name)
+       # #1 we save the index of the leftmost item in the array in a variable named lower, and the index of rightmost item in the array in upper. 
+       # #  If we think of the array in terms of left-to-right where the leftmost item is the zeroth index and the rightmost item is the entries.length-1 index.
+     lower = 0
+     upper = entries.length - 1
+     # #2 we loop while our lower index is less than or equal to our upper index.
+     while lower <= upper
+       # #3 we find the middle index by taking the sum of lower and upper and dividing it by two. 
+       # #  Ruby will truncate any decimal numbers, so if upper is five and lower is zero then mid will get set to two. Then we retrieve the name of the entry at the middle index and store it in mid_name.
+       mid = (lower + upper) / 2
+       mid_name = entries[mid].name
+ 
+       # #4 we compare the name that we are searching for, name, to the name of the middle index, mid_name. 
+       # #  We use the == operator when comparing the names which makes the search case sensitive
+       if name == mid_name
+         return entries[mid]
+       elsif name < mid_name
+         upper = mid - 1
+       elsif name > mid_name
+         lower = mid + 1
+       end
+     end
+     # #5 f we divide and conquer to the point where no match is found, we return nil.
+     return nil
+   end
 end
